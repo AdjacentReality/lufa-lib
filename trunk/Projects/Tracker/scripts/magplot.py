32557fd61e75ecbd5e8b6b8bb634cc154d7edc8a
Nirav Patel <nrp@eclecti.cc>
2012-05-02 23:58:45 -0700

    write out samples to a file

--- trunk/Projects/Tracker/scripts/magplot.py
+++ trunk/Projects/Tracker/scripts/magplot.py
@@ -4,13 +4,16 @@ import matplotlib.pyplot as plt
 from serialtest import *
 
 port = '/dev/ttyACM0'
+out_file = 'samples.txt'
 if (len(sys.argv) > 1):
     port = sys.argv[1]
+if (len(sys.argv) > 2):
+    out_file = sys.argv[2]
 tracker = Tracker(port)
 x = []
 y = []
 z = []
-num_samples = 10000
+num_samples = 5000
 while len(x) < num_samples:
     packet = tracker.read_packet()
     if packet[0] == PACKET_MAG:
@@ -18,10 +21,20 @@ while len(x) < num_samples:
         y.append(packet[2])
         z.append(packet[3])
     if not len(x) % 100:
-        print "%f%%" % (100.0*(len(x)/float(num_samples)))
+        print "%f%%" % (100.0*(float(len(x))/float(num_samples)))
+
+f = open(out_file, 'w')
+f.write("x = ")
+f.write(str(x))
+f.write(";\ny = ")
+f.write(str(y))
+f.write(";\nz = ")
+f.write(str(z))
+f.write(";\n")
+f.close()
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+ax = fig.add_subplot(111, projection='3d', aspect='equal')
 ax.scatter(x, y, z)
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
